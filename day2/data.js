76d70f321754a19a35b8dd511faedb3aabc0baa7
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-22 17:18:51 +0300

    add readfile referesher in node js

---
+++ day2/data.js
@@ -0,0 +1,6 @@
+const sum =(num1,num2)=>{
+    return num1+ num2;
+
+}
+const val=sum(12,12)
+console.log(val)
---
+++ day2/readfile1.js
@@ -0,0 +1,12 @@
+const fs=require("fs")
+fs.readFile("data.js","utf8",(error,data)=>{
+ if(error){
+     console.log(`somthing happend! `,error)
+       return
+    }  
+  else{
+    console.log("congratulation")
+    console.log("Welcome read file1")
+  }
+    
+})
