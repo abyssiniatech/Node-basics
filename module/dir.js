47236335f7fe1e649cd4b7deeaf3bde118b0f9b1
abyssiniatech <surafelmengist2015@gmail.com>
2026-04-01 19:36:01 +0300

    add dir name

---
+++ module/dir.js
@@ -0,0 +1,5 @@
+// _dir
+// __dirname is a global variable that holds the directory name of the current module. It is a string that represents the absolute path of the directory containing the currently executing file. This variable is useful for constructing file paths and accessing files relative to the current module's location.
+// 
+const path = require("path");
+console.log(__dirname);
