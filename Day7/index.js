a5b3211443a89f3ee249bd4b90415fbe5e7b405d
abyssiniatech <surafelmengist2015@gmail.com>
2026-04-01 20:23:07 +0300

    add common js

--- Day7/index.js
+++ Day7/index.js
@@ -1,3 +1,7 @@
-const fs=require('./math.js').default
+const data = require('./data.js');
 
-console.log("Hello World")
+console.log("Hello World");
+
+data.map((item) => {
+    console.log(`My name is ${item.name} and I am ${item.age} years old. I live in ${item.city}.`);
+});
