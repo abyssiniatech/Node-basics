41ff9ef8731b279043c4752d06f14fbdd9be1568
abyssiniatech <surafelmengist2015@gmail.com>
2026-04-04 11:23:51 +0300

    add tintermidate

--- Day8/calc.js
+++ Day8/calc.js
@@ -1,13 +1,5 @@
-const add =(a,b)=>a+b
-const subtract =(a,b)=>a-b
-const multiply =(a,b)=>a*b
-const divide =(a,b)=>a/b
-const modulo=(a,b)=>a%b
-
-module.exports={
-    add,
-    subtract,
-    multiply,
-    divide,
-    modulo
-}
+module.exports.add = (a, b) => a + b;
+module.exports.subtract = (a, b) => a - b;
+module.exports.multiply = (a, b) => a * b;
+module.exports.divide = (a, b) => a / b;
+module.exports.modulo = (a, b) => a % b;
