f99106ae6d868d1f33054139749330a5af16336a
abyssiniatech <surafelmengist2015@gmail.com>
2026-04-10 10:06:47 +0300

    add node os

---
+++ 05/os.js
@@ -0,0 +1,9 @@
+const os=require('os');
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.totalmem());
+console.log(os.freemem());
+console.log(os.hostname());
+console.log(os.release());
+console.log(os.userInfo());
+console.log(os.cpus());
