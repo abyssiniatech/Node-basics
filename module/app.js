d60c83f033d3ecd45e464d559d811b4bf125978f
abyssiniatech <surafelmengist2015@gmail.com>
2026-04-13 19:26:46 +0300

    add module in node js

--- module/app.js
+++ module/app.js
@@ -1,4 +1,7 @@
 const sum=require('./math.js');
+require('./batman.js');
+require('./superman.js');
 console.log(sum(5,10))
 console.log("Hello World")
+
 
---
+++ module/batman.js
@@ -0,0 +1,2 @@
+const batman="batman";
+console.log(batman);
