92fef2f8221f0677f3b7cc4e60a165b1692af6f9
abyssiniatech <surafelmengist2015@gmail.com>
2026-04-14 11:20:09 +0300

    add folder inside 06

--- 06/file.js
+++ 06/file.js
@@ -1,2 +1,25 @@
 const path=require('path');
-console.log(path.parse(__filename))
+const fs=require('fs')
+// console.log(path.parse(__filename))
+
+// absolute
+// console.log(path.isAbsolute(__filename))
+// fs.mkdir("newdir",(err)=>{
+//     if(err){
+//         console.log(err)
+//     }
+    
+//         console.log("successfully new folder was created")
+    
+
+// })
+
+
+// create sura.txt inside 
+fs.writeFile("welcome.txt","welcome to surafel",(err)=>{
+    if(err){
+        console.log(err)
+    }
+    console.log("seccessfully create welcome")
+})
+
