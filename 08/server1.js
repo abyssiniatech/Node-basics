58d601d8506824216457a45e8b7ad5f045ab1815
abyssiniatech <surafelmengist2015@gmail.com>
2026-04-16 17:29:58 +0300

    add server

--- 08/server1.js
+++
@@ -1,9 +0,0 @@
-const http=require('http');
-
-const server=http.createServer((req,res)=>{
-    res.write("these is server")
-    res.end()
-})
-server.listen(1000,"localhost",()=>{
-    console.log("these website is listen on : http://localhost:1000")
-})
