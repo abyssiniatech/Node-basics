61fa997d7b85ba0429da2cab48d80e3da9803485
abyssiniatech <surafelmengist2015@gmail.com>
2026-04-16 15:13:49 +0300

    add simple route in node js

--- 08/senddata.js
+++ 08/senddata.js
@@ -1,29 +1,30 @@
 const fs = require('fs');
 const http = require('http');
-const path="./view/"
+
 const server = http.createServer((req, res) => {
     res.setHeader("Content-Type", "text/html");
+
+    let filePath = "./view/"; // ✅ use let (new variable per request)
+
     switch (req.url) {
         case '/':
-            path+="index.html";
+            filePath += "index.html";
             break;
-
         case '/about':
-            path+="index.html";
+            filePath += "about.html";
             break;
-    
         default:
-            path+= "404.html"
+            filePath += "404.html";
             break;
     }
 
-    fs.readFile(path, (err, data) => {
+    fs.readFile(filePath, (err, data) => {
         if (err) {
             console.log(err);
             res.statusCode = 500;
             res.end("Server Error");
         } else {
-            res.end(data); // send HTML and end response
+            res.end(data);
         }
     });
 });
