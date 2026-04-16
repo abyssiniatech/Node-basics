84073d6db6745127ce76cf6fd0c09f901881bf18
abyssiniatech <surafelmengist2015@gmail.com>
2026-04-16 13:56:33 +0300

    add node fundamentals

--- 08/app.js
+++ 08/app.js
@@ -1,12 +1,12 @@
 const express=require('express');
 const app=express()
 app.get("/",(req,res)=>{
-    res.setHeader("content-type","plain/text")
-    res.write("i am surafel")
+    res.write("<h1>Welcome to node js</h1>")
+    res.write("<p>this is node js crash course</p>")
     res.end()
 })
 
 // listen the server
-app.listen(2000,"localhost",()=>{
-    console.log("http://localhost:2000")
+app.listen(5000,()=>{
+    console.log("http://localhost:5000")
 })
