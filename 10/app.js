a70a4fa53a34fca3d90c14f2ad05d238f724f9fb
abyssiniatech <surafelmengist2015@gmail.com>
2026-04-22 12:41:06 +0300

    add esmodule

--- 10/app.js
+++ 10/app.js
@@ -1,4 +1,10 @@
 const val=require('./math.js')
+import modu from "./module"
+
 // return the result of the 
 const res=val(12,23)
 console.log(res);
+
+
+// esmodule 
+
---
+++ 10/module.js
@@ -0,0 +1,6 @@
+export const modu=(a)=>{
+    return a*10
+}
+
+const m=modu(12)
+console.log(m)
