6adef19b0f9235a3cc6a490ae1ff43fafce85612
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-01 10:48:19 +0300

    add path module in node js

---
+++ 17/path.js
@@ -0,0 +1,10 @@
+const path=require('path');
+const fs=require('fs');
+console.log(__filename);
+console.log(__dirname);
+
+
+
+console.log("-----------------------------")
+console.log(path.basename(__dirname));
+console.log(path.dirname(__filename));
