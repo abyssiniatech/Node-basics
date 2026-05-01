de57a3ba5251ac8bf9d85b640c1240dba3da6644
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-01 10:51:43 +0300

    add the second path :

--- 17/path.js
+++ 17/path.js
@@ -8,3 +8,9 @@ console.log(__dirname);
 console.log("-----------------------------")
 console.log(path.basename(__dirname));
 console.log(path.dirname(__filename));
+
+
+
+console.log("-----------------------------")
+console.log(path.extname(__filename));
+console.log(path.extname(__dirname));
