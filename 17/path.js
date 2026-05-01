0bc59f6ca24af56b22e7535409f7b30f06d9e122
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-01 10:55:25 +0300

    add  common js path:

--- 17/path.js
+++ 17/path.js
@@ -14,3 +14,14 @@ console.log(path.dirname(__filename));
 console.log("-----------------------------")
 console.log(path.extname(__filename));
 console.log(path.extname(__dirname));
+
+
+console.log("-----------------------------")
+console.log(path.parse(__filename));
+
+
+console.log("-----------------------------")
+console.log(path.join(__dirname,'/test','/test1','/test2','/test3','/test4','/test5','/test6','/test7','/test8','/test9','/test10'));
+
+console.log("-----------------------------")
+console.log(path.resolve(__dirname,'/test','/test1','/test2','/test3','/test4','/test5','/test6','/test7','/test8','/test9','/test10'));
