afc7ded7fdfc41b6ccee9526e069af56adb1971d
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-08 11:14:20 +0300

    modify events:

--- 22/event.js
+++ 22/event.js
@@ -1,7 +1,9 @@
 const eventEmmiter=require("events");
 const event= new eventEmmiter();
-event.on("datas",()=>{
+event.on("datas",(id,name)=>{
     console.log("these is the first events over the net")
+    console.log(`my name is ${name}`)
+    console.log(`my Id number is: ${id}`)
 })
 
-event.emit("datas")
+event.emit("datas",12,"surafel")
