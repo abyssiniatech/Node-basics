986a69fd7f8970740d4698ff7e8bf7285afe02f2
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-10 15:46:45 +0300

    add the express server:

--- 24/server.js
+++ 24/server.js
@@ -1,10 +1,22 @@
-const http=require("http");
-const server=http.createServer((req,res)=>{
+// const http=require("http");
+const express =require('express');
+// const server=http.createServer((req,res)=>{
     
-    res.write("welcome to http server")
-    res.end("End")
+//     res.write("welcome to http server")
+//     res.end("End")
+// })
+
+// server.listen(3000,()=>{
+//     console.log("run at https://localhost:3000")
+// })
+
+
+// express server
+const app=express();
+app.get("/",(req,res)=>{
+    res.write("welcome to express server")
 })
 
-server.listen(3000,()=>{
-    console.log("run at https://localhost:3000")
+app.listen(2000,()=>{
+    console.log("welcome")
 })
