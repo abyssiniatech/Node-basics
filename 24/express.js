0d5a026151dd567d9f5f6be653e2b5c46f52b270
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-10 16:25:25 +0300

    add the server:"

--- 24/express.js
+++ 24/express.js
@@ -3,14 +3,14 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req, res) => {
-    res.status((200).send('<h1>welcome express</h1>'))
-
-
+    res.status(200).send('<h1>Express Server</h1>')
 })
 
-app.use((req,res)=>{
-     res.status((404).send('<h1>404 page not found</h1>'))
+// 404 handler
+app.use((req, res) => {
+    res.status(404).send('404 page not found')
 })
+
 app.listen(5000, () => {
     console.log("Server running at http://localhost:5000")
 })
