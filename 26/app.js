e2e3c5beb811849e6c9490a0b83ab7b1f158fba7
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-11 19:41:02 +0300

    add the second professional route:

--- 26/app.js
+++ 26/app.js
@@ -1,27 +1,13 @@
-const http=require('http');
-const port=3000;
-const host='localhost';
- 
-const server=http.createServer((req,res)=>{
-    const url=req.url;
-     if(req.url ==='/'){
-         res.writeHead(200,{'content-type':'text/html'});
-         res.write('<p>Home page!</p>')
-         res.end()
-     }
-       else if(req.url ==='/about'){
-         res.writeHead(200,{'content-type':'text/html'});
-         res.write('<p>About page!</p>')
-         res.end()
-     }
-       else{
-         res.writeHead(404,{'content-type':'text/html'});
-         res.write('<p>Page not found!</p>')
-         res.end()
-     }
-})
+const http = require('http');
+const routes = require('./routes');
 
-// create port
-server.listen(port,host,()=>{
-    console.log(`https://${host}:${port}`)
-})
+const PORT = 3000;
+const HOST = 'localhost';
+
+const server = http.createServer(routes);
+
+server.listen(PORT, HOST, () => {
+
+    console.log(`Server running at http://${HOST}:${PORT}`);
+
+});
