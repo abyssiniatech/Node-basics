da00c493c62f139f91e651b0dc6ac6d23e19a390
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-11 19:12:13 +0300

    add route inn node js :

--- 26/app.js
+++ 26/app.js
@@ -1,9 +1,24 @@
 const http=require('http');
 const port=3000;
 const host='localhost';
+ 
 const server=http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/html'});
-    res.write('<p>Welcome to my website</p>')
+    const url=req.url;
+     if(req.url ==='/'){
+         res.writeHead(200,{'content-type':'text/html'});
+         res.write('<p>Home page!</p>')
+         res.end()
+     }
+       else if(req.url ==='/about'){
+         res.writeHead(200,{'content-type':'text/html'});
+         res.write('<p>About page!</p>')
+         res.end()
+     }
+       else{
+         res.writeHead(404,{'content-type':'text/html'});
+         res.write('<p>Page not found!</p>')
+         res.end()
+     }
 })
 
 // create port
