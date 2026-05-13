3946ae90e30f764fadfe6cd2f7b4ab1e8298568a
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-13 15:30:36 +0300

    add the json in node js :"

--- 27/app.js
+++ 27/app.js
@@ -3,7 +3,10 @@ const app=express()
 app.get('/',(req,res)=>{
     res.status(200).send("Home page!")
 })
-app.all('*').send("404 page not found")
+
+app.all("*",(req,res)=>{
+    res.status(404).send("<h1>404 page not found</h1>")
+})
 
 
 
