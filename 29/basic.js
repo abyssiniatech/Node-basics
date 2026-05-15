e739c34142ad9ea94fa0bbc1d5190f4bb17cf9f1
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-15 23:56:08 +0300

    add file operations in node js

--- 29/basic.js
+++ 29/basic.js
@@ -1,5 +1,36 @@
 // basic node js 
+const fs=require("node:fs")
 const os= require('os');
 console.log(os.version())
 console.log(os.type())
 console.log(os.homedir())
+
+// file system in node js 
+fs.readFile("file.txt",'utf-8',(err,data)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log(data)
+    }
+})
+
+// writeFile in node js
+fs.writeFile("file.txt","my name is surafel",(err)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log("Congratulation are successfuly created:")
+    }
+})
+
+// delete file
+fs.unlink("app.txt",(err)=>{
+    if(err){
+       console.log(err)
+    }
+    else{
+      console.log("you are deleted seccesfully!")
+    }
+})
